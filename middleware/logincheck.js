8d3285c3f61d336e4ac04fd7167923ccbe70ff73
Shankar Anbalagan <bravoshankar@gmail.com>
2019-09-27 14:54:30 +0530

    Authenticator added and readme updated

--- middleware/logincheck.js
+++ middleware/logincheck.js
@@ -2,11 +2,12 @@ var User=require('./../models/Users');
 
 module.exports=function(){
     return function(req,res,next){
+        var usertoken=req.body.usertoken;
         User.findOne({userName:'admin', isLoggedIn:true},function(err,data){
-            if(data)
+            if(data && data.userToken===usertoken)
                 next();
             else{
-                res.status(422).json({status:'fail',data:{message:"User not logged in or does not exist"}});
+                res.status(422).json({status:'fail',data:{message:"User not logged in or user token invalid"}});
             }
         });
     };
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
-#Certificate Verification System
+# Certificate Verification System
+
+To run this, clone it and
+
+```bash
+cd certificate-verify
+npm i
+npm run dev-start
+```
+The server will be available on localhost:3000/
