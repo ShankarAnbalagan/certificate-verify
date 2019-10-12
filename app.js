6a4f904462a812f999f321b92268e682e1f9af98
Shankar Anbalagan <bravoshankar@gmail.com>
2019-10-12 10:10:03 +0530

    cors support added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var dotenv = require('dotenv');
 var mongoose = require('mongoose');
+var cors=require('cors');
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
